283d812148ec8001fc09fe0754871b147f16f420
masud <masudranaacb@gmail.com>
2024-02-23 14:22:31 +0600

    ES6 intro

---
+++ api.js
@@ -0,0 +1,19 @@
+
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+    console.log(sum, num1, num2)
+    return sum
+}
+// const result=add(2,5)
+
+const funllName = (first, last) => first + last
+const result = funllName("ali", "hosssin")
+console.log(result)
+// console.log(result)
+const numbers = function (num1, num2) {
+    return num1 + num2
+}
+const numbersResult=numbers(2,5)
+console.log(numbersResult)
+
