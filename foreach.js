d214e63b02e5dfd72edfc1acfcbb6ab3ab43ce74
masud <masudranaacb@gmail.com>
2024-02-23 22:16:39 +0600

    foreach,filter,find

---
+++ foreach.js
@@ -0,0 +1,9 @@
+const numbers=[12,5,6,8,6]
+const newNumbers=numbers.forEach(num=>num*2)
+// console.log(newNumbers)
+const friend=["ali","jamal","kamal","saju"]
+const lengt4Charr=friend.filter(friend=>friend.length>4)
+// console.log(lengt4Charr)
+const number=[,5,6,9,63,89,6,9]
+const selectedNumber=number.find(num=>num>10)
+console.log(selectedNumber)
---
+++ map.js
@@ -0,0 +1,8 @@
+const numbers=[2,8,6.5,8,9,]
+
+const result=numbers.map(num=>num*2)
+// console.log(result)
+
+const frinds=["alim","jalim","surjo","Sakib"]
+const myFristLatt=frinds.map(char=>[char,char.length])
+console.log(myFristLatt)
