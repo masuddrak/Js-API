0a7ed46f78d4047c7c82f37b0494f63ba1b6d97f
masud <masudranaacb@gmail.com>
2024-02-23 23:21:51 +0600

    basic class introduction

---
+++ class.js
@@ -0,0 +1,9 @@
+class Teacher{
+    constructor(name,subject){
+        this.name=name,
+        this.subject=subject
+    }
+}
+const subinSir=new Teacher("Subin","Physice")
+const mrubSir=new Teacher("Marup","Chemistry")
+console.log(mrubSir)
