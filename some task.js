8d4633b345e1be82d1431e2e49038383879cb271
masud <masudranaacb@gmail.com>
2024-02-25 01:41:09 +0600

    frist task complet

---
+++ some task.js
@@ -0,0 +1,11 @@
+
+// for lop way
+const arr=[1,3,5,7,9]
+let eventArr=[]
+for(let i=0; i<arr.length; i++){
+    eventArr.push(arr[i]+1)
+}
+// console.log(eventArr)
+// array map mathod using 
+const newArray=arr.map(number=>number+1)
+console.log(newArray)
