b26ce80c9d16ffc56e0785366f39fcb93a75fba8
masud <masudranaacb@gmail.com>
2024-02-24 18:05:57 +0600

    callback function details

---
+++ callback function.js
@@ -0,0 +1,10 @@
+ function nameHan(name){
+    mosiorHanelar(name)
+}
+
+function mosiorHanelar(name){
+    console.log("hello ",name)
+}
+ const mosior= nameHan("sakib")
+ const mosiorJaman= nameHan("Jaman")
+ console.log(mosiorJaman)
