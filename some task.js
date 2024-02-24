fcd8443d83ef2a1c6f9ef0a062f16dbb2c00b35d
masud <masudranaacb@gmail.com>
2024-02-25 01:57:26 +0600

    taks 3

--- some task.js
+++ some task.js
@@ -1,3 +1,7 @@
-const arr=[33,50,79,78,90,101,30]
-const divisable10Arr=arr.filter(number=>number%10==0)
-console.log(divisable10Arr)
+const instructor=[
+    {name:"nodi",age:28, position:"senior"},
+    {name:"akil",age:28, position:"junior"},
+    {name:"shobuj",age:28, position:"senior"},
+]
+const seniorInstructors=instructor.filter(instrac=>instrac.position==="senior")
+console.log(seniorInstructors)
