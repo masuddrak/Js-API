9283bfde60096ea8d8f0a5c7dbc0449a36746394
masud <masudranaacb@gmail.com>
2024-02-24 18:45:33 +0600

    premitive and non premitive example

--- callback function.js
+++ callback function.js
@@ -1,10 +1,18 @@
- function nameHan(name){
-    mosiorHanelar(name)
+
+const num1 = 5;
+const num2 = 2;
+function add(num1, num2) {
+    num1 = 6
+    console.log(num1 + num2)
+}
+// add(num1,num2)
+
+function multiply(numbers){
+    numbers[0]=55
+    console.log(numbers);
 }
 
-function mosiorHanelar(name){
-    console.log("hello ",name)
-}
- const mosior= nameHan("sakib")
- const mosiorJaman= nameHan("Jaman")
- console.log(mosiorJaman)
+const numbers = [1, 96, 9, 6, 7]
+console.log(numbers)
+const newNumbers= multiply(numbers)
+console.log(newNumbers)
